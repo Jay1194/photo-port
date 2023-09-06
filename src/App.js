88dba3cbd36d0313bdadbf489d443878412a13c6
Jay <j.thomastt1@gmail.com>
2023-09-06 16:56:29 -0600

    created Nav and about me components

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
-
-import './App.css';
+import React from 'react';
+import Nav from './components/Nav/index';
+import About from './components/About/index';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-     
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Nav></Nav>
+      <main>
+       <About></About>
+      </main>
     </div>
   );
 }
---
+++ src/components/Nav/index.js
@@ -0,0 +1,61 @@
+import React from 'react';
+
+// create objects that contain each category's name and a short description. That way, we can use that same data elsewhere in the app.
+const categories = [
+    {
+        name: "commercial",
+        description:
+        "Photos of grocery stores, food trucks, and other commercial projects",
+    },
+    
+    {    name: "portraits", description: "Portraits of people in my life" },
+    {    name: "food", description: "Delicious delicacies" },
+    {
+        name:"landscape",
+        description: "Fields, farmhouses, waterfalls, and the beuty of nature",
+    },
+];
+
+// define onclick statement
+function categorySelected(name) {
+    //It's important that we wrap it in a function declaration rather than just calling categorySelected(category.name)
+    console.log(`${name} clicked`);
+}
+
+const Nav = () => {
+
+    return(
+    <header className='flex-row px-1'>
+        <h2>
+            <a href="/">
+                <span role="img" aria-label="camera"> 📸</span> Oh Snap!
+            </a>
+        </h2>
+        <nav>
+            <ul className="flex-row">
+                <li className="mx-2">
+                    <a href="#about">
+                        About me
+                    </a>
+                </li>
+                <li>
+                  <span>Contact</span>
+                </li>
+                {categories.map((category) => (
+                    <li
+                    className="mx-1"
+                    key={category.name}
+                    >
+                        <span onClick={() => categorySelected(category.name)} >
+                            {category.name}
+                        </span>
+                    </li>
+                ))}
+            </ul>
+        </nav>
+    </header>
+    
+    );
+}
+
+export default Nav;
